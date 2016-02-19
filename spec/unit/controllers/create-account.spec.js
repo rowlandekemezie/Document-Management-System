1e0f3ef3b87658f840b47daeab56ecdea3ca3c3c
andela-rekemezie <rowland.ekemezie@andela.com>
2016-02-19 07:16:26 +0100

    Front End Tests Refers to [#114067957] - Add unit tests for account countrollers

---
+++ spec/unit/controllers/create-account.spec.js
@@ -0,0 +1,116 @@
+(function() {
+  'use strict';
+
+  describe('UserAcountCtrl tests', function() {
+    var scope,
+      controller,
+      state,
+      Auth,
+      Utils,
+      Users = {
+        login: function(user, cb) {
+          cb(!user, {
+            user: {
+              name: 3,
+              email: 'great',
+              _id: 1
+            }
+          });
+        },
+        save: function(user, cb) {
+          if(user){
+          cb(user);
+        } else{
+          cb({
+            res: {
+              err: 'Could not respond',
+              status: 500
+            }
+          });
+        }
+      }
+      };
+
+    beforeEach(function() {
+      module('docKip');
+    });
+
+    beforeEach(inject(function($injector) {
+      var $controller = $injector.get('$controller');
+      scope = $injector.get('$rootScope');
+      controller = $controller('UserAccountCtrl', {
+        $scope: scope,
+        Users: Users
+      });
+      Auth = $injector.get('Auth');
+      state = $injector.get('$state');
+      Utils = $injector.get('Utils');
+    }));
+
+    it('should call the save function on signup', function() {
+      spyOn(Users, 'save').and.callThrough();
+      spyOn(Auth, 'setToken').and.callThrough();
+      spyOn(state, 'go').and.callThrough();
+      spyOn(scope, 'loginUser').and.callThrough();
+      scope.user = {
+        firstName: 'Abu',
+        lastName: 'lulu',
+        userName: 'LuluAbu',
+        email: 'abulul@gmail.com',
+        role: 'Trainer',
+        password: 'Bummerick',
+        confirmPassword: 'Bummerick'
+      };
+      scope.signUp();
+      expect(Users.save).toHaveBeenCalled();
+      expect(Auth.setToken).toHaveBeenCalled();
+      expect(state.go).toHaveBeenCalled();
+      expect(scope.loggedInUser).toBeDefined();
+      expect(scope.loginUser).toHaveBeenCalled();
+    });
+
+    it('should call the save function unsuccesfully', function() {
+      spyOn(Users, 'save').and.callThrough();
+      spyOn(Auth, 'setToken');
+      spyOn(state, 'go');
+      scope.user = null;
+      scope.signUp();
+      expect(Users.save).toHaveBeenCalled();
+      expect(Auth.setToken).not.toHaveBeenCalled();
+      expect(state.go).not.toHaveBeenCalled();
+      expect(scope.loggedInUser).not.toBeDefined();
+      expect(scope.status).toBeDefined();
+    });
+
+    it('should call login function on loginUser', function() {
+      spyOn(Users, 'login').and.callThrough();
+      spyOn(Auth, 'setToken');
+      spyOn(state, 'go');
+      spyOn(Utils, 'toast');
+      scope.user = true;
+      scope.loginUser();
+      expect(Users.login).toBeDefined();
+      expect(Users.login).toHaveBeenCalled();
+      expect(Auth.setToken).toHaveBeenCalled();
+      expect(state.go).toHaveBeenCalled();
+      expect(scope.loggedInUser).toBeDefined();
+      expect(Utils.toast).toHaveBeenCalled();
+    });
+
+    it('should call login function unsuccesfully', function() {
+      spyOn(Users, 'login').and.callThrough();
+      spyOn(Auth, 'setToken');
+      spyOn(state, 'go');
+      spyOn(Utils, 'toast');
+      scope.user = null;
+      scope.loginUser();
+      expect(Users.login).toBeDefined();
+      expect(Users.login).toHaveBeenCalled();
+      expect(Auth.setToken).not.toHaveBeenCalled();
+      expect(state.go).not.toHaveBeenCalled();
+      expect(scope.loggedInUser).not.toBeDefined();
+      expect(scope.status).toBeDefined();
+      expect(Utils.toast).not.toHaveBeenCalled();
+    });
+  });
+})();
---
+++ spec/unit/controllers/header.spec.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+
+  describe('HeadCtrl tests', function() {
+
+    var scope,
+      controller,
+      Users = {
+        logout: function(cb) {
+          if (scope.loggedInUser) {
+            cb(null, true);
+          } else {
+            cb(true, null);
+          }
+        }
+      },
+      state,
+      route,
+      Auth;
+
+    beforeEach(function() {
+      module('docKip');
+    });
+
+    beforeEach(inject(function($injector) {
+      var $controller = $injector.get('$controller');
+      scope = $injector.get('$rootScope');
+      controller = $controller('HeadCtrl', {
+        $scope: scope,
+        Users: Users
+      });
+      Auth = $injector.get('Auth');
+      state = $injector.get('$state');
+      route = $injector.get('$route');
+    }));
+
+    it('should define and call Users.logout', function() {
+      scope.loggedInUser = 1;
+      spyOn(Users, 'logout').and.callThrough();
+      spyOn(Auth, 'logout').and.callThrough();
+      spyOn(state, 'go').and.callThrough();
+      spyOn(scope, 'loggedIn').and.callThrough();
+      spyOn(route, 'reload').and.callThrough();
+      scope.logoutUser();
+      expect(Users.logout).toHaveBeenCalled();
+      expect(Auth.logout).toHaveBeenCalled();
+      expect(state.go).toHaveBeenCalled();
+      expect(scope.loggedIn).toBeTruthy();
+      expect(route.reload).toHaveBeenCalled();
+    });
+
+    it('should define and call Users.logout', function() {
+      scope.loggedInUser = null;
+      spyOn(Users, 'logout').and.callThrough();
+      spyOn(Auth, 'logout').and.callThrough();
+      spyOn(state, 'go').and.callThrough();
+      spyOn(scope, 'loggedIn').and.callThrough();
+      spyOn(route, 'reload').and.callThrough();
+      // spyOn(console, 'log').and.callThrough();
+      scope.logoutUser();
+      expect(Users.logout).toHaveBeenCalled();
+      expect(Auth.logout).not.toHaveBeenCalled();
+      expect(state.go).not.toHaveBeenCalled();
+      expect(route.reload).not.toHaveBeenCalled();
+      expect(scope.loggedIn).toBeTruthy();
+      //expect(console.log).toHaveBeenCalled();
+    });
+  });
+})();
