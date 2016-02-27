7f09abc914b018ea134abe9a3b10fc7dc75060e2
andela-rekemezie <rowland.ekemezie@andela.com>
2016-02-27 09:20:17 +0100

    Update travis configurations Refers to [#114487833] - update bower.json - update package.json

--- seeds/user.js
+++ seeds/user.js
@@ -1,57 +1,51 @@
-// (function() {
-//   'use strict';
+(function() {
+  'use strict';
 
-//   var Roles = require('../server/api/models/role.model'),
-//     Users = require('../server/api/models/user.model');
+  var Roles = require('../server/api/models/role.model'),
+    Users = require('../server/api/models/user.model');
 
-//   function report(err) {
-//     if (err) {
-//       console.log('There was a problem seeding a user.');
-//     } else {
-//       console.log('Successfully seeded SuperAdmin user.');
-//     }
-//   }
-//   var role;
-//   var initDb = function() {
+  function report(err) {
+    if (err) {
+      console.log('There was a problem seeding a user.');
+    } else {
+      console.log('Successfully seeded SuperAdmin user.');
+    }
+  }
+  var role;
+  var initDb = function() {
 
-//     Roles.find({}).exec(function(err, roles) {
-//       if (err) {
-//         console.log('There was problem seeding roles');
-//       } else {
-//         if (roles.length === 0) {
-//           var roleData = {
-//             title: 'SuperAdmin'
-//           };
-//           var newRole = new Roles(roleData);
-//           role = newRole.save();
-//           seedUser();
-//         }
-//       }
-//     });
-//   };
+    var roleData = {
+      title: 'SuperAdmin'
+    };
+    var newRole = new Roles(roleData);
+    newRole.save(function(err, roles) {
+      role = roles;
+      seedUser();
+    });
+  };
 
-//   function seedUser() {
-//     Users.find({}).exec(function(err, users) {
-//       if (err) {
-//         console.log('There was problem seeding user');
-//       } else {
-//         if (users.length === 0) {
-//           var userData = {
-//             name: {
-//               firstName: 'Buddy',
-//               lastName: 'Master'
-//             },
-//             role: 'SuperAdmin',
-//             userName: 'BuddyMaster',
-//             password: 'ApplicationOwner',
-//             email: 'buddymaster@dockip.com'
-//           };
-//           var newUser = new Users(userData);
-//           newUser.save(report);
-//         }
-//       }
-//     });
-//   }
+  function seedUser() {
+    Users.find({}).exec(function(err, users) {
+      if (err) {
+        console.log('There was problem seeding user');
+      } else {
+        if (users.length === 0) {
+          var userData = {
+            name: {
+              firstName: 'Buddy',
+              lastName: 'Master'
+            },
+            role: role.title,
+            userName: 'BuddyMaster',
+            password: 'owner123',
+            email: 'buddymaster@dockip.com'
+          };
+          var newUser = new Users(userData);
+          newUser.save(report);
+        }
+      }
+    });
+  }
 
-//   module.exports = initDb;
-// })();
+  module.exports = initDb;
+})();
