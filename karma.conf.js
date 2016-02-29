a714d8887cc7f100eb9756ee9659fac36b26888a
andela-rekemezie <rowland.ekemezie@andela.com>
2016-02-29 11:28:23 +0100

    Update Karma reporter [Delivers #114663033] - Add karma reporter to karma file

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress', 'junit', 'growl',
     // 'coverage', 'spec', 'failed'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'coverage', 'progress'],
+    reporters: [ 'coverage', 'progress', 'coveralls'],
 
     // web server port
     port: 9876,
